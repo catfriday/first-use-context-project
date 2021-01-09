e417293bec679eb9f175d0f2fd38e5bafc35d932
Catrina Friday <cat.friday@gmail.com>
2021-01-09 18:10:34 -0500

    added open and close modal funtionality

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from './logo.svg'
 import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
+import {useGlobalContext} from './context'
 
 const Sidebar = () => {
   return <aside className={`sidebar `}>
@@ -13,7 +14,7 @@ const Sidebar = () => {
     </div>
     <ul className='links'>
       {links.map(link =>{
-        const{id, url, text, icon} = link
+        const {id, url, text, icon} = link
         return <li key={id}>
           <a href={url}>
             {icon}
